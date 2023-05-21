25976c6a9c722d6da7f94754a2f229dbe61f2899
xiaofan4122 <847964640@qq.com>
2023-05-22 00:02:50 +0800

    first commit

---
+++ update.bat
@@ -0,0 +1,6 @@
+cd F:\AERO\Blog\my-website
+F:
+git config --global user.name "xiaofan4122"
+git config --global user.email "847964640@qq.com"
+set "USE_SSH=true" && yarn deploy
+@REM set "GIT_USER=xiaofan4122" && yarn deploy
